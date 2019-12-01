93ba42157e3af5873e6766a3893ddc0edac82f2d
nativedevlopper6 <53089601+nativedevlopper6@users.noreply.github.com>
2019-12-01 19:26:53 +0100

    Start Interface

---
+++ app/src/main/java/com/example/floppybird/GameView.java
@@ -0,0 +1,35 @@
+package com.example.floppybird;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.View;
+
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
+
+public class GameView extends View {
+
+    //this our custume view class
+ Handler handler;
+ Runnable runnable;
+
+    public GameView(Context context) {
+        super(context);
+      handler = new Handler();
+      runnable = new Runnable() {
+          @Override
+          public void run() {
+          invalidate();
+          }
+      };
+
+    }
+@Override
+     protected void  onDraw(Canvas canvas){
+        super.onDraw(canvas);
+      //We'll draw our view inside onDraw()
+
+     handler.postDelayed(runnable.Update_MILLES )
+}
+
+}
