ad137527a554bed8b3cd0d188c3d01838a1fecf6
AmeniRaboudi <ameni.rabboudi@gmail.com>
2019-12-02 23:42:06 +0100

    Start Interface

---
+++ app/src/main/java/com/example/floppybird/le_bird_entre_le_pipe.java
@@ -0,0 +1,14 @@
+package com.example.floppybird;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class le_bird_entre_le_pipe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_le_bird_entre_le_pipe);
+    }
+}
---
+++ app/src/main/java/com/example/floppybird/select_bird.java
@@ -0,0 +1,14 @@
+package com.example.floppybird;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class select_bird extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_bird);
+    }
+}
